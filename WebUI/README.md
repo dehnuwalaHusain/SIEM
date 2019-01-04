a5cf250f5112d8c1b993570d487d3a074e5bf153
dehnuwalaHusain <dehnuwala.husain@gmail.com>
2019-01-04 23:20:43 +0530

    Removed old WebUI

--- WebUI/README.md
+++
@@ -1,1 +0,0 @@
-This folder contains the webUI code that'll be accessible from the system on which OSSEC server is installed.
--- WebUI/includes/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost:3307','root','');
-	if( !$conn ) {
-		
-		echo "Connection Failed";
-	}
-    mysqli_select_db( $conn, "agentmaster");
-?>
